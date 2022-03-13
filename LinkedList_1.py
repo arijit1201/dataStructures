bd1a85683f09d9f1a2f8bbbb40326b944c618678
arijit1201 <arijit1201@gmail.com>
2022-03-14 01:48:13 +0530

    dsa practice

--- LinkedList_1.py
+++ LinkedList_1.py
@@ -11,6 +11,7 @@ class LinkedList:
         self.head=None
     
     def printList(self):
+        print("-----------------------------------")
         temp = self.head
         while(temp):
             print(temp.data)
@@ -20,6 +21,22 @@ class LinkedList:
         new_node = Node(new_data)
         new_node.next = self.head
         self.head = new_node
+    def insertAfterGivenNode(self, previousNode, data):
+        if previousNode is None :
+            print("previous Node must exist")
+            return
+        new_node = Node(data)
+        new_node.next = previousNode.next
+        previousNode.next = new_node
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:      #self.head refers to the head Node
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next is not None :
+            temp = temp.next
+        temp.next = new_node
 
 if __name__=='__main__':
     llist = LinkedList()
@@ -33,3 +50,12 @@ if __name__=='__main__':
     llist.printList()
 
     llist.insertAtFront(45)
+    llist.printList()
+
+    #print(llist.head, llist.head.next, llist.head.next.next)
+    llist.insertAfterGivenNode(llist.head.next.next, 69)
+    llist.printList()
+
+    llist.insertAtEnd(96)
+    llist.insertAtEnd(969)
+    llist.printList()
